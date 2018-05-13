9c7851a5f2a238a9771048f3a8f1f983807670da
Niro Liyanage <niro.liyanage@touchcorp.com>
2018-05-14 07:46:33 +1000

    remove

--- deploy/env.sh
+++ deploy/env.sh
@@ -1,8 +1,8 @@
 export TF_VAR_region="ap-southeast-2"
 export region="ap-southeast-2"
 export tfplan="sinatra.tfplan"
-export TF_VAR_access_key="AKIAJD7AK4DHKUQSM2RA"
-export TF_VAR_secret_key="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"
+export TF_VAR_access_key="xxx"
+export TF_VAR_secret_key="xxxx+LE9/LU"
 export TF_VAR_cloudflare_email="niroshan.d.liyanage@gmail.com"
-export TF_VAR_cloudflare_token="7f73c28c62f8b45a7d2e57caf83cc9f8b69ed"
+export TF_VAR_cloudflare_token="xxxx"
 
