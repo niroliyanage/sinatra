cbdf030b7e061bc2dbf224d46d9a17e3b130e2ff
Niro Liyanage <niro.liyanage@touchcorp.com>
2018-05-15 23:01:56 +1000

    key pair

--- deploy/env.sh
+++ deploy/env.sh
@@ -1,8 +1,9 @@
 export TF_VAR_region="ap-southeast-2"
 export region="ap-southeast-2"
 export tfplan="sinatra.tfplan"
-export TF_VAR_access_key="AKIAIUMASPTDKG5P6LJA"
-export TF_VAR_secret_key="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"
+export TF_VAR_access_key="xxxx"
+export TF_VAR_secret_key="xxxx"
 export TF_VAR_cloudflare_email="niroshan.d.liyanage@gmail.com"
-export TF_VAR_cloudflare_token="61a25ddc1f37d177274fef9964f031a0f8318"
+export TF_VAR_cloudflare_token="xxxxx"
+export TF_VAR_key_pair_name="testing"
 
